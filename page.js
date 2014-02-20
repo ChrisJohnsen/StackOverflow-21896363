06fa14547d15e6597c083875eea17f6c085eccbd
Chris Johnsen <chris_johnsen@pobox.com>
2014-02-20 13:38:47 -0600

    Code for SO 21896363

--- page.js
+++ page.js
@@ -1,1 +1,70 @@
-// code goes here
+function so21896363() {
+/*
+/* Posted Code Starts Here
+ */
+var fileHandler = {
+    entry: null,
+    open: function(cb){
+
+        chrome.fileSystem.chooseEntry({ type: 'openDirectory' }, 
+        function(dirEntry) {
+
+            if (!dirEntry || !dirEntry.isDirectory) {
+                cb && cb(null);
+                return;
+            }
+
+            this.entry = dirEntry;
+
+            var reader = this.entry.createReader();
+            reader.readEntries(function(entries) {
+              for (var i = 0; i < entries.length; ++i) {
+                console.log("entry is " + entries[i].fullPath);
+              }
+            }, function(){
+                console.log("error");
+            });
+
+        });
+
+
+    },
+
+    save: function(filename, source) {
+
+        chrome.fileSystem.getWritableEntry(this.entry, function(entry) {
+
+            entry.getFile(filename, {create:true}, function(entry) {
+                entry.createWriter(function(writer) {
+                    writer.write(new Blob([source], {type: 'text/javascript'}));
+                });
+            });
+
+        });
+
+    },
+
+    saveAs: function(filename, source) {
+
+        chrome.fileSystem.chooseEntry({type:'openDirectory'}, 
+        function(entry) {
+
+            chrome.fileSystem.getWritableEntry(entry, function(entry) {
+
+                entry.getFile(filename, {create:true}, function(entry) {
+                    entry.createWriter(function(writer) {
+                        writer.write(new Blob([source], {type: 'text/javascript'}));
+                    });
+                });
+
+            });
+        });
+
+    }
+}
+/*
+ * Posted Code Ends Here
+ */
+}
+
+so21896363();
